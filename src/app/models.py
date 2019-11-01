f37af771ecf79338705633b0b7cd1ecdf8d15ee3
martonur <mrm4rc1@gmail.com>
2019-11-01 08:26:04 +0100

    ADD: price to pizza model

--- src/app/models.py
+++ src/app/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
 class Pizza(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    price = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
