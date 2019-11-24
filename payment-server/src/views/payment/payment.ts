98e61038d5d6f45d3b051937dff17f6cec3ff595
Attila Tóth <muchcoffe@gmail.com>
2019-11-24 14:09:54 +0100

    added new logic

--- payment-server/src/views/payment/payment.ts
+++ payment-server/src/views/payment/payment.ts
@@ -3,11 +3,32 @@ import './payment.scss'
 const successButton = document.getElementById('success-button')
 const failureButton = document.getElementById('failure-button')
 
+enum REDIRECT_STATUS {
+  SUCCESS = 'success',
+  FAILURE = 'failure'
+}
+
+const determineRedirectUri = (): string => window.location.href.split('=')[1]
+
+const redirectWithStatus = (status: REDIRECT_STATUS): void => {
+  let redirectUri = ''
+  try {
+    redirectUri = determineRedirectUri()
+  } catch (e) {
+    console.error('Unable to determine redirect uri!')
+    console.error(e.toString())
+    return
+  }
+
+  window.location.href = redirectUri.concat(`?status=${status}`)
+}
+
 successButton?.addEventListener('click', (e: MouseEvent): void => {
   e.preventDefault()
-  // todo: change this ordenáré solution
-  const redirectUri = successButton.getAttribute('data-redirect')
-  if (!redirectUri) return
+  redirectWithStatus(REDIRECT_STATUS.SUCCESS)
+})
 
-  window.location.href = redirectUri
+failureButton?.addEventListener('click', (e: MouseEvent) => {
+  e.preventDefault()
+  redirectWithStatus(REDIRECT_STATUS.FAILURE)
 })
