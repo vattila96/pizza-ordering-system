47611181d4738b310efc8af675b7fe5fe32906c3
Attila Tóth <attila.toth@syrg.app>
2019-12-08 22:08:49 +0100

    updated editorconfig and did some styling

--- src/app/views.py
+++ src/app/views.py
@@ -17,9 +17,11 @@ from .forms import *
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 30)]
 
+
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
 
 @require_POST
 def cart_add(request, product_id):
@@ -30,8 +32,9 @@ def cart_add(request, product_id):
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
     else:
-      cart.add(product=product, quantity=1)
+        cart.add(product=product, quantity=1)
     return redirect('shoppingcart')
+
 
 def cart_remove(request, product_id):
     cart = Cart(request)
@@ -39,16 +42,19 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('shoppingcart')
 
+
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
     return render(request, 'shoppingcart.html', {'cart': cart})
 
+
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
 
 def index(request):
     home_page_detail = HomePageDetail.objects.last()
@@ -83,20 +89,21 @@ def pizzalist(request):
     if request.method == 'POST':
         keyword = request.POST.get("custompizza_name", None)
         if keyword != "":
-          pizza_price = request.POST.get("custompizza_price", None)
-          Pizza(name=keyword, description="Custom pizza", is_custom_pizza=True, price=pizza_price).save()
+            pizza_price = request.POST.get("custompizza_price", None)
+            Pizza(name=keyword, description="Custom pizza", is_custom_pizza=True, price=pizza_price).save()
 
     pizzas = Pizza.objects.all().order_by('-name')
     pizza_categories = PizzaCategory.objects.all().order_by('-name')
     context = {'pizzalist_page': 'active', 'pizzas': pizzas, 'categories': pizza_categories}
     return render(request, 'pizzalist.html', context)
 
+
 def categoryfilter(request):
     category = request.POST.get("category", None)
     if category == "all":
-      pizzas = Pizza.objects.all().order_by('-name')
+        pizzas = Pizza.objects.all().order_by('-name')
     else:
-      pizzas = Pizza.objects.filter(category__name=category)
+        pizzas = Pizza.objects.filter(category__name=category)
     pizza_categories = PizzaCategory.objects.all().order_by('-name')
     context = {'pizzalist_page': 'active', 'pizzas': pizzas, 'categories': pizza_categories}
     return render(request, 'pizzalist.html', context)
@@ -111,6 +118,7 @@ def pizzasearch(request):
     context = {'pizzalist_page': 'active', 'pizzas': pizzas, 'categories': pizza_categories}
     return render(request, 'pizzalist.html', context)
 
+
 def pizzareset(request):
     keyword = request.POST.get("reset_keyword", None)
     pizzas = Pizza.objects.all().order_by('-name')
@@ -118,30 +126,32 @@ def pizzareset(request):
     context = {'pizzalist_page': 'active', 'pizzas': pizzas, 'categories': pizza_categories}
     return render(request, 'pizzalist.html', context)
 
+
 def myorders(request):
     myorders = []
 
-    for order in Order.objects.filter(O_T_M_User_Orders = request.user):
-      sum = 0
-      actorderitems = []
-      for orderitem in OrderItem.objects.filter(O_T_M_Order_OrderItems = order):
-        for food in FoodProduct.objects.filter(orderitems = orderitem):
-          actorderitems.append(food)
-          sum += food.price
-        for drink in DrinkProduct.objects.filter(orderitems = orderitem):
-          actorderitems.append(drink)
-          sum += drink.price
+    for order in Order.objects.filter(O_T_M_User_Orders=request.user):
+        sum = 0
+        actorderitems = []
+        for orderitem in OrderItem.objects.filter(O_T_M_Order_OrderItems=order):
+            for food in FoodProduct.objects.filter(orderitems=orderitem):
+                actorderitems.append(food)
+                sum += food.price
+            for drink in DrinkProduct.objects.filter(orderitems=orderitem):
+                actorderitems.append(drink)
+                sum += drink.price
 
-      try:
-        myorders.append((actorderitems, sum, order.transaction.currency, order))
-      except Exception:
-        myorders.append((actorderitems, sum, "HUF", order))
+        try:
+            myorders.append((actorderitems, sum, order.transaction.currency, order))
+        except Exception:
+            myorders.append((actorderitems, sum, "HUF", order))
 
-      myorders = myorders[-3:]
+        myorders = myorders[-3:]
 
     context = {"myorders_page": "active",
                "myorders": myorders}
     return render(request, 'myorders.html', context)
+
 
 @login_required
 @transaction.atomic
